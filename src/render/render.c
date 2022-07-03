9a3b7343eb7f224a7e444ad013e80754e23cc87e
HoneyPony <honeyponygames@gmail.com>
2022-07-02 23:44:30 -0400

    Start implementing framebuffer
    The primary missing things at this point are:
    - Properly scaling the framebuffer for correct high-quality pixel art
      rendering. In particular, the framebuffer will be drawn at an exact
      integer multiple of its size on the screen. If screen size / buffer
      size is not an integer, the framebuffer will be drawn slightly larger
      than the screen, so that some framebuffer pixels are not fully visible.
    - Not resizing the framebuffer every single frame.

--- src/render/render.c
+++ src/render/render.c
@@ -27,7 +27,7 @@ struct {
 struct {
 	GLuint shader;
 	GLuint tex;
-} framebuffer_shader;
+} frame_shader;
 
 static mat4 projection;
 
@@ -39,16 +39,30 @@ static GLuint tex;
 
 float vertices[] = {
 	0.f,  0.f,  0.f,    0.f, 0.f,
-	0.f,  10.f, 0.f,    0.f, 1.f,
-	10.f, 10.f, 0.f,    1.f, 1.f,
+	0.f,  16.f, 0.f,    0.f, 1.f,
+	16.f, 16.f, 0.f,    1.f, 1.f,
 
-	10.f, 10.f, 0.f,    1.f, 1.f,
+	16.f, 16.f, 0.f,    1.f, 1.f,
 	0.f,  0.f,  0.f,    0.f, 0.f,
-	10.f, 0.f,  0.f,    1.f, 0.f
+	16.f, 0.f,  0.f,    1.f, 0.f
 };
 
-static GLuint vao, vbo;
+float fb_verts[] = {
+	-1.f, -1.f,    0.f, 0.f,
+	-1.f,  1.f,    0.f, 1.f,
+	 1.f,  1.f,    1.f, 1.f,
 
+	 1.f,  1.f,    1.f, 1.f,
+	-1.f, -1.f,    0.f, 0.f,
+	 1.f, -1.f,    1.f, 0.f
+};
+
+static GLuint vbo;
+
+static GLuint fb_vbo;
+
+static int screen_width, screen_height;
+static int frame_width, frame_height; // TODO: Consolidate all variables in one place
 
 static void init_ctx() {
 	glGenFramebuffers(1, &ctx.pix_framebuffer);
@@ -73,6 +87,25 @@ static void init_ctx() {
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
+
+	int status;
+	if((status = glCheckFramebufferStatus(GL_FRAMEBUFFER)) != GL_FRAMEBUFFER_COMPLETE) {
+		printf("[ponygame] framebuffer status incomplete: %x\n", status);
+	}
+}
+
+void resize_framebuffer() {
+	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pix_framebuffer);
+
+	glBindTexture(GL_TEXTURE_2D, ctx.pix_tex);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ctx.pix_tex, 0);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, frame_width, frame_height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
 }
 
 void render_init() {
@@ -88,6 +121,12 @@ void render_init() {
 	sprite_shader.mat = shader_name(sprite_shader.shader, "transform");
 	sprite_shader.tex = shader_name(sprite_shader.shader, "tex");
 
+	frame_shader.shader = shader_compile(shader_src_frame_vert, shader_src_frame_frag);
+	frame_shader.tex = shader_name(frame_shader.shader, "texture");
+
+	frame_width = 480; // TODO: Do initial computation with window size...?
+	frame_width = 360;
+
 	//glGenVertexArrays(1, &vao);
 	//glBindVertexArray(vao);
 
@@ -95,21 +134,27 @@ void render_init() {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	
+	glGenBuffers(1, &fb_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, fb_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(fb_verts), fb_verts, GL_STATIC_DRAW);
 
-	mat4_ortho(&projection,
-		-20, 20,
-		-20, 20,
-		-10, 10);
-	//init_ctx();
+	init_ctx();
 	tex = gltex_load("res://test_sprite.png");
 }
 
 void render_fit_window(int width, int height) {
-	glViewport(0, 0, width, height);
+	screen_width = width;
+	screen_height = height;
 
-	float x = width;
-	float y = height;
+	frame_width = width / 4;
+	frame_height = height / 4;
+
+	resize_framebuffer();
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//glViewport(0, 0, width, height);
+
+	float x = frame_width;//width;
+	float y = frame_height;//height;
 	x *= 0.5;
 	y *= 0.5;
 
@@ -120,15 +165,22 @@ void render_fit_window(int width, int height) {
 }
 
 void render() {
+	
+
+	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pix_framebuffer);
+	glViewport(0, 0, frame_width, frame_height);
+	//glViewport(0, 0, 480, 360);
+
+
 	glClearColor(0.1, 0.1, 0.1, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(sprite_shader.shader);
 
-	
-
 	shader_set_mat4(sprite_shader.mat, &projection);
 	shader_set_int(sprite_shader.tex, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	// TODO: Implement this more broadly
 	// Details: it appears that the minimal subset of WebGL supported by
@@ -145,7 +197,31 @@ void render() {
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, tex);
+
+	
 	
 	//glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, screen_width, screen_height);
+
+	glUseProgram(frame_shader.shader);
+
+	shader_set_int(frame_shader.tex, 0);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, ctx.pix_tex);
+
+	glBindBuffer(GL_ARRAY_BUFFER, fb_vbo);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	
+
+	
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 }
