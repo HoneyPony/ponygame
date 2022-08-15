d91bd2836997836b6994875257493af69dcb7422
HoneyPony <honeyponygames@gmail.com>
2022-08-15 17:52:18 -0400

    Work on naming conventions

--- src/render/render.c
+++ src/render/render.c
@@ -9,15 +9,25 @@
 #include "shader_src.h"
 
 typedef struct {
-	// The framebuffer used for all pixel graphics operations. This
-	// framebuffer is rendered with nearest neighbor filtering to display
-	// pixel-perfect art on screen.
-	GLuint pix_framebuffer;
-	GLuint pix_tex;
-	GLuint pix_depth_stencil_rbo;
+	// This data is associated with the framebuffer used to render most game
+	// content to. It is set to the exact pixel size of the game, which is then
+	// scaled up with nearest neighbor filtering to fill the screen.
+	struct {
+		GLuint framebuffer;
+		GLuint color_tex;
+		GLuint depth_stencil_rbo;
 
-	
-} RenderCtx;
+		GLuint rect_vbo;
+	} pixel_fb;
+
+	mat4 projection;
+
+	int32_t screen_width;
+	int32_t screen_height;
+
+	int32_t frame_width;
+	int32_t frame_height;
+} RenderContext;
 
 struct {
 	GLuint shader;
@@ -30,10 +40,10 @@ struct {
 	GLuint tex;
 } frame_shader;
 
-static mat4 projection;
 
 
-static RenderCtx ctx;
+
+static RenderContext ctx;
 
 static GLuint tex;
 
@@ -48,7 +58,7 @@ float vertices[] = {
 	17.f, 1.f,  1.f,    1.f, 0.f
 };
 
-float fb_verts[] = {
+float pixel_fb_verts[] = {
 	-1.f, -1.f,    0.f, 0.f,
 	-1.f,  1.f,    0.f, 1.f,
 	 1.f,  1.f,    1.f, 1.f,
@@ -60,18 +70,13 @@ float fb_verts[] = {
 
 static GLuint vbo;
 
-static GLuint fb_vbo;
-
-static int screen_width, screen_height;
-static int frame_width, frame_height; // TODO: Consolidate all variables in one place
-
 static void init_ctx() {
-	glGenFramebuffers(1, &ctx.pix_framebuffer);
+	glGenFramebuffers(1, &ctx.pixel_fb.framebuffer);
 
-	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pix_framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pixel_fb.framebuffer);
 
-	glGenTextures(1, &ctx.pix_tex);
-	glBindTexture(GL_TEXTURE_2D, ctx.pix_tex);
+	glGenTextures(1, &ctx.pixel_fb.color_tex);
+	glBindTexture(GL_TEXTURE_2D, ctx.pixel_fb.color_tex);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 480, 360, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 
@@ -84,10 +89,10 @@ static void init_ctx() {
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ctx.pix_tex, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ctx.pixel_fb.color_tex, 0);
 
-	glGenRenderbuffers(1, &ctx.pix_depth_stencil_rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
+	glGenRenderbuffers(1, &ctx.pixel_fb.depth_stencil_rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, ctx.pixel_fb.depth_stencil_rbo);
 	// GLES2: Very limited render buffer format selection. GL_DEPTH_COMPONENT16 is the only depth one available.
 	// We could consider using GLES3, maybe, for better selection...?
 	// Could also consider using a texture instead of a renderbuffer, but I would have
@@ -95,7 +100,7 @@ static void init_ctx() {
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, 480, 360);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, ctx.pixel_fb.depth_stencil_rbo);
 
 	int status;
 	if((status = glCheckFramebufferStatus(GL_FRAMEBUFFER)) != GL_FRAMEBUFFER_COMPLETE) {
@@ -104,17 +109,17 @@ static void init_ctx() {
 }
 
 void resize_framebuffer() {
-	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pix_framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pixel_fb.framebuffer);
 
-	glBindTexture(GL_TEXTURE_2D, ctx.pix_tex);
+	glBindTexture(GL_TEXTURE_2D, ctx.pixel_fb.color_tex);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ctx.frame_width, ctx.frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ctx.pix_tex, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ctx.pixel_fb.color_tex, 0);
 
-	glBindRenderbuffer(GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, frame_width, frame_height);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, ctx.pix_depth_stencil_rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, ctx.pixel_fb.depth_stencil_rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, ctx.frame_width, ctx.frame_height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, ctx.pixel_fb.depth_stencil_rbo);
 }
 
 void render_init() {
@@ -133,8 +138,8 @@ void render_init() {
 	frame_shader.shader = shader_compile(shader_src_frame_vert, shader_src_frame_frag);
 	frame_shader.tex = shader_name(frame_shader.shader, "texture");
 
-	frame_width = 480; // TODO: Do initial computation with window size...?
-	frame_width = 360;
+	ctx.frame_width = 480; // TODO: Do initial computation with window size...?
+	ctx.frame_height = 360;
 
 	//glGenVertexArrays(1, &vao);
 	//glBindVertexArray(vao);
@@ -143,9 +148,9 @@ void render_init() {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glGenBuffers(1, &fb_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, fb_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(fb_verts), fb_verts, GL_STATIC_DRAW);
+	glGenBuffers(1, &ctx.pixel_fb.rect_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, ctx.pixel_fb.rect_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pixel_fb_verts), pixel_fb_verts, GL_STATIC_DRAW);
 
 	init_ctx();
 	tex = gltex_load("res://test_sprite.png");
@@ -163,13 +168,13 @@ void compute_screen_vertices() {
 	// 
 	// In particular, we can divide the "overextended"
 
-	float scale_x = (float)(frame_width * 4) / (float)(screen_width);
-	float scale_y = (float)(frame_height * 4) / (float)(screen_height);
+	float scale_x = (float)(ctx.frame_width * 4) / (float)(ctx.screen_width);
+	float scale_y = (float)(ctx.frame_height * 4) / (float)(ctx.screen_height);
 
 	scale_x = 1.0 + ((scale_x - 1.0) / 2.0);
 	scale_y = 1.0 + ((scale_y - 1.0) / 2.0);
 
-	logf_info("width: %d vs %d", frame_width * 4, screen_width);
+	logf_info("width: %d vs %d", ctx.frame_width * 4, ctx.screen_width);
 
 	float posx = scale_x;
 	float negx = -scale_x;
@@ -177,67 +182,65 @@ void compute_screen_vertices() {
 	float negy = -scale_y;
 
 	// First triangle
-	fb_verts[0] = negx;
-	fb_verts[1] = negy;
+	pixel_fb_verts[0] = negx;
+	pixel_fb_verts[1] = negy;
 
-	fb_verts[4] = negx;
-	fb_verts[5] = posy;
+	pixel_fb_verts[4] = negx;
+	pixel_fb_verts[5] = posy;
 
-	fb_verts[8] = posx;
-	fb_verts[9] = posy;
+	pixel_fb_verts[8] = posx;
+	pixel_fb_verts[9] = posy;
 
 	logf_info("x scale, y scale: %f %f", scale_x, scale_y);
 
 	// Second triangle
-	fb_verts[12] = posx;
-	fb_verts[13] = posy;
+	pixel_fb_verts[12] = posx;
+	pixel_fb_verts[13] = posy;
 
-	fb_verts[16] = negx;
-	fb_verts[17] = negy;
+	pixel_fb_verts[16] = negx;
+	pixel_fb_verts[17] = negy;
 
-	fb_verts[20] = posx;
-	fb_verts[21] = negy;
+	pixel_fb_verts[20] = posx;
+	pixel_fb_verts[21] = negy;
 
 	// Upload buffer
-	glBindBuffer(GL_ARRAY_BUFFER, fb_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(fb_verts), fb_verts, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, ctx.pixel_fb.rect_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pixel_fb_verts), pixel_fb_verts, GL_STATIC_DRAW);
 }
 
 void render_fit_window(int width, int height) {
-	if(width == screen_width && height == screen_height) return;
+	if(width == ctx.screen_width && height == ctx.screen_height) return;
 
-	screen_width = width;
-	screen_height = height;
+	ctx.screen_width = width;
+	ctx.screen_height = height;
 
 
-	frame_width = (width + 3) / 4; // Need to effectively take ceiling of these
+	ctx.frame_width = (width + 3) / 4; // Need to effectively take ceiling of these
 	                               // dimensions, due to compute_screen_vertices, etc
-	frame_height = (height + 3) / 4;
+	ctx.frame_height = (height + 3) / 4;
 
 	resize_framebuffer();
 	compute_screen_vertices();
 
-	float x = frame_width;
-	float y = frame_height;
+	float x = ctx.frame_width;
+	float y = ctx.frame_height;
 
-	mat4_ortho(&projection,
+	mat4_ortho(&ctx.projection,
 		0, x,
 		0, y,
 		-10, 10); // TODO: Determine Z range
 }
 
-void render() {
-	
-
-	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pix_framebuffer);
-	glViewport(0, 0, frame_width, frame_height);
+void render_game_objects() {
+	glBindFramebuffer(GL_FRAMEBUFFER, ctx.pixel_fb.framebuffer);
+	glViewport(0, 0, ctx.frame_width, ctx.frame_height);
 
 	glClearColor(0.3, 0.3, 0.3, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(sprite_shader.shader);
 
-	shader_set_mat4(sprite_shader.mat, &projection);
+	shader_set_mat4(sprite_shader.mat, &ctx.projection);
 	shader_set_int(sprite_shader.tex, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -259,17 +262,19 @@ void render() {
 	glBindTexture(GL_TEXTURE_2D, tex);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
 
+void render_framebuffer_to_screen() {
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	glViewport(0, 0, screen_width, screen_height);
+	glViewport(0, 0, ctx.screen_width, ctx.screen_height);
 
 	glUseProgram(frame_shader.shader);
 
 	shader_set_int(frame_shader.tex, 0);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, ctx.pix_tex);
+	glBindTexture(GL_TEXTURE_2D, ctx.pixel_fb.color_tex);
 
-	glBindBuffer(GL_ARRAY_BUFFER, fb_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, ctx.pixel_fb.rect_vbo);
 
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -278,4 +283,9 @@ void render() {
 	glEnableVertexAttribArray(1);
 	
 	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void render() {
+	render_game_objects();
+	render_framebuffer_to_screen();	
 }
