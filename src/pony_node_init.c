6079c69be382b06e4437979b8a77f8c22ee26dd9
HoneyPony <honeyponygames@gmail.com>
2022-08-18 03:01:11 -0400

    Add transform init to Node constructor

--- src/pony_node_init.c
+++ src/pony_node_init.c
@@ -7,8 +7,12 @@ node_meta_defines(Node)
 void construct_Node(void *node) {
 	Node *self = node;
 
+	// Need to initialize the parent-child structure.
 	self->parent = NULL;
 	ls_init(self->children);
+
+	// The transformation should be initialized to the identity.
+	raw_transform_copy(&self->raw_tform, raw_transform_identity());
 }
 
 void pony_init_builtin_nodes() {
