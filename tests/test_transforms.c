c65c0d5c10dc1e6964ff6bc4641f22d4d7de09b5
HoneyPony <honeyponygames@gmail.com>
2022-08-22 00:33:10 -0400

    Add another transform test

--- tests/test_transforms.c
+++ tests/test_transforms.c
@@ -33,10 +33,30 @@ test_define(test_transform_45_deg_parent, {
 
 	vec2 gpos = get_gpos(child);
 
-	test_assert(gpos.x == gpos.y, "Child x should equal y. Instead, x = %f, y = %f", gpos.x, gpos.y)
+	test_assert(gpos.x == gpos.y, "Child x should equal y. Instead, x = %f, y = %f", gpos.x, gpos.y);
+})
+
+// TODO: Implement vector math
+bool equals(vec2 a, vec2 b) {
+	return (a.x == b.x) && (a.y == b.y);
+}
+
+test_define(test_scale_local_to_global, {
+	Node *center = node_new(Node);
+
+	set_lscale(center, vxy(3.0, 2.0));
+
+	vec2 a = local_to_global(center, vxy(1.0, 1.0));
+	vec2 b = local_to_global(center, vxy(-1.0, -2.0));
+	vec2 c = local_to_global(center, vxy(3.5, -2.25));
+
+	test_assert(equals(a, vxy(3.0, 2.0)),   "expected  3.0,  2.0 got %f, %f\n", a.x, a.y);
+	test_assert(equals(b, vxy(-3.0, -4.0)), "expected -3.0, -4.0 got %f, %f\n", b.x, b.y);
+	test_assert(equals(c, vxy(10.5, -4.5)), "expected 10.5, -4.5 got %f, %f\n", c.x, c.y);
 })
 
 test_set_define(test_set_transforms, {
 	test_transform_basic_offset();
 	test_transform_45_deg_parent();
+	test_scale_local_to_global();
 })
