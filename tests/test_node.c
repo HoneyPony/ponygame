70ce54fb305e5224d9a820340a4939fa84148c17
HoneyPony <honeyponygames@gmail.com>
2022-08-23 23:51:58 -0400

    Add tests for node constructor and destructor

--- tests/test_node.c
+++ tests/test_node.c
@@ -14,13 +14,65 @@ int grandchild;
 FieldList_Node \
 int unrelated_child;
 
+#define FieldList_ConstructDestruct \
+FieldList_Node \
+int int_var; \
+list_of(int) list_var;
+
 node_from_field_list(Child)
 node_from_field_list(Grandchild)
 node_from_field_list(UnrelatedChild)
+node_from_field_list(ConstructDestruct)
 
 node_meta_defines(Child)
 node_meta_defines(Grandchild)
 node_meta_defines(UnrelatedChild)
+node_meta_defines(ConstructDestruct)
+
+#define TEST_CONSTRUCT_INT_VAL 10
+#define TEST_DESTRUCT_INT_VAL 20
+#define TEST_CONSTRUCT_LIST_LENGTH 5
+
+void construct_ConstructDestruct(void *ptr) {
+	ConstructDestruct *self = ptr;
+
+	self->int_var = TEST_CONSTRUCT_INT_VAL;
+	ls_init(self->list_var);
+
+	for(int i = 0; i < TEST_CONSTRUCT_LIST_LENGTH; ++i) {
+		ls_push(self->list_var, i * 2);
+	}
+}
+
+void destruct_ConstructDestruct(void *ptr) {
+	ConstructDestruct *self = ptr;
+
+	self->int_var = TEST_DESTRUCT_INT_VAL;
+	ls_free(self->list_var);
+	self->list_var = NULL;
+}
+
+test_define(test_node_constructor, {
+	ConstructDestruct *cd = new(ConstructDestruct);
+	test_assert_eq(TEST_CONSTRUCT_INT_VAL, cd->int_var, "Expected var initialized to %d, got %d\n");
+
+	if(ls_length(cd->list_var) != TEST_CONSTRUCT_LIST_LENGTH) {
+		test_assert(0, "List initialized with incorrect length");
+	}
+	else {
+		for(int i = 0; i < ls_length(cd->list_var); ++i) {
+			test_assert_eq(i * 2, cd->list_var[i], "Expected list member initialized to %d, got %d\n");
+		}
+	}
+})
+
+test_define(test_node_destructor, {
+	ConstructDestruct *cd = new(ConstructDestruct);
+	node_destroy(cd);
+
+	test_assert_eq(TEST_DESTRUCT_INT_VAL, cd->int_var, "Expected var initialized to %d, got %d\n");
+	test_assert(cd->list_var == NULL, "Expected null list var, got %p\n", cd->list_var);
+})
 
 test_define(test_node_downcast, {
 	Child *test = new(Child);
@@ -72,6 +124,13 @@ test_set_define(test_set_node, {
 	node_meta_initialize(Child, &node_header(Node), NULL, NULL, NULL);
 	node_meta_initialize(Grandchild, &node_header(Child), NULL, NULL, NULL);
 	node_meta_initialize(UnrelatedChild, &node_header(Node), NULL, NULL, NULL);
+	node_meta_initialize(ConstructDestruct, &node_header(Node),
+		construct_ConstructDestruct,
+		NULL,
+		destruct_ConstructDestruct)
+
+	test_node_constructor();
+	test_node_destructor();
 
 	test_node_downcast();
 	test_node_downcast_grandchild();
